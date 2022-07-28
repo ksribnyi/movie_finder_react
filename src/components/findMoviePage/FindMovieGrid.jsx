c5badb70108b6315b5dd98cba32e78ec23fc5339
kosty1998 <kosty81998@gmail.com>
2022-07-28 14:33:58 +0300

    link on poster to imdb

--- src/components/findMoviePage/FindMovieGrid.jsx
+++ src/components/findMoviePage/FindMovieGrid.jsx
@@ -1,14 +1,18 @@
 import {Button, TextField} from "@mui/material";
 import React from "react";
 import GridMovie from "./GridMovie";
+import "./FindMovie.css"
 
 const FindMovieGrid = (props) => {
     return (
         <div>
-            <TextField variant={"outlined"} size={"small"} style={{background: "grey", borderRadius: 5}}
-                       autoComplete={"off"} onChange={props.onChange} value={props.bodyInput}/>
-            <Button style={{background: "grey", borderRadius: 5, height: 40, color: "black"}}
-                    onClick={props.findMovie}>FIND</Button>
+            <div className={"find_movie_control"}>
+                <TextField variant={"outlined"} size={"small"} style={{background: "grey", borderRadius: 5}}
+                           autoComplete={"off"} onChange={props.onChange} value={props.bodyInput}/>
+                <Button style={{background: "grey", borderRadius: 5, height: 40, color: "black", width:55}}
+                        onClick={props.findMovie}>FIND</Button>
+            </div>
+
             <GridMovie movie={props.movie}/>
 
         </div>
--- src/components/findMoviePage/GridMovie.jsx
+++ src/components/findMoviePage/GridMovie.jsx
@@ -1,22 +1,28 @@
 import {Grid} from "@mui/material";
 import React from "react";
+import "./FindMovie.css"
 
 const GridMovie = (props) => {
     const imdbLink = "https://www.imdb.com/title/"
     return (
         <div>
-            <Grid container rowSpacing={2} columnSpacing={3}
-                  style={{margin: "auto", maxWidth: 1240, textAlign: "center"}} >
+            <Grid container rowSpacing={2} columnSpacing={2}
+                  className={'grid_movie'}>
                 {props.movie.map((row) => (
-                    <Grid key={row.id} item >
-                        <div style={{background: "gray", width:280, height:500}}><div align="right">{row.poster ?
-                            <img style={{width: 280, height:450, textAlight: "center"}} alt='poster movie'
-                                 src={row.poster}/> : 'No photo'}</div>
+                    <Grid key={row.id} item>
+                        <div style={{background: "gray", width: 280, height: 500, marginRight: 16}}>
+                            <div align="right">{row.poster ?
+                                <a href={imdbLink + row.imdb_id + '/'}>
+                                    <img style={{width: 280, height: 450, textAlight: "center"}} alt='poster movie'
+                                         src={row.poster}/>
+                                </a> : 'No photo'}</div>
                             <div align="right">{
-                                <a style={{textDecoration: "none", wordWrap: "break-word"}} href={imdbLink + row.imdb_id + '/'}
+                                <a style={{textDecoration: "none", wordWrap: "break-word"}}
+                                   href={imdbLink + row.imdb_id + '/'}
                                    target="_blank"
                                    rel="noreferrer noopener">{row.title}</a>}</div>
-                            <div align="right">{row.year}</div></div>
+                            <div align="right">{row.year}</div>
+                        </div>
                     </Grid>
                 ))}
             </Grid>
